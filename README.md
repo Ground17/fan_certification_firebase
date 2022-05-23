b62250b6fc120c86e982247c5b93b20b4ec7d199
jisang-dev <hyla981020@gmail.com>
2022-05-23 16:31:39 +0900

    initial upload, edited README.md

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # fan_certification_firebase
 firebase functions and hosting for fan_certification
+
+## 0. testing on local by running emulator
+[Document](https://firebase.google.com/docs/functions/local-emulator)
+
+## 1-1. functions
+- addHeart
+    - input: platform(*String*, default value: "0"), account(*String*, ChannelID for YouTube)
+    - function: increases the like by 1 when 5 minutes or more pass from the 'recent' using the 'recent' inside the 'celeb', or outputs an error to try again after a while
+
+- manageFollow
+    - input: platform(*String*, default value: "0"), account(*String*, ChannelID for YouTube), method(*String*, "add", "update" or "delete"), title(*String?*, channel name), profileURL(*String?*, thumbnail url)
+    - function: 
+        - add
+            adds celebrities to 'celeb' in my account. length of 'celeb' cannot be more than three and values of 'celeb' cannot be duplicated.
+
+        - update
+            updates title and url of celeb. Currently, there are Title and url for each celeb, but it will be integrated into one later. See to-do list.
+
+        - delete (doesn't need title and url)
+            deletes celebrities in 'celeb' in user's account and decrease related celeb's follow and likes
+
+    **To-do list**
+    [ ] **(important)** In the case of title and url, it is not saved for each celebrity in each account, but it is saved in a celebrity on "YouTube" or "Instagram" collection to eliminate duplication.
+
+- initUser
+    - input: None
+    - function: sets default values such as "celeb: []" if user is not in "Users" collection
+
+- deleteAccount
+    - input: None
+    - function: deletes user's account in the "Users" collection and initializes all user have followed and clicked likes
+
+## 1-2. output structure of functions
+- case success
+    - {status: 200, message: "success"}
+- case error
+    - {status: 400, message: "{message of error}"}
+
+## 2. hosting (Getting ready...)
---
+++ functions/index.js
@@ -0,0 +1,299 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// const https = require('https');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.addHeart = functions.https.onCall(async (data, context) => {
+    const platform = data.platform;
+    const account = data.account;
+
+    // Checking attribute.
+    if (!(typeof platform === 'string') || platform.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "platform" containing the message text to add.');
+    }
+    if (!(typeof account === 'string') || account.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "account" containing the message text to add.');
+    }
+
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+            'while authenticated.');
+    }
+
+    if (platform == "0") { // YouTube
+        // transaction
+        const YouTubeRef = admin.firestore().collection('YouTube').doc(account);
+        const myRef = admin.firestore().collection('Users').doc(context.auth.uid);
+        try {
+            return await admin.firestore().runTransaction(async (t) => {
+                const YouTubeDoc = await t.get(YouTubeRef);
+                const myDoc = await t.get(myRef);
+
+                let celeb = myDoc.data().celeb;
+                let hearts = YouTubeDoc.data().count + 1;
+                
+                let isAdded = false;
+                for (let i = 0; i < celeb.length; i++) {
+                    if (celeb[i].account == account && celeb[i].platform == platform) {
+                        isAdded = true;
+                        const now = new Date();
+                        if ((celeb[i].recent.seconds + 300) * 1000 < now.getTime()) {
+                            // Note: this could be done without a transaction
+                            //       by updating the population using FieldValue.increment()
+                            t.update(YouTubeRef, {count: hearts});
+    
+                            celeb[i].recent = now;
+                            celeb[i].count = celeb[i].count + 1;
+                            t.update(myRef, {celeb: celeb});
+                            break;
+                        } else {
+                            throw 'Sorry! Try after 5 minutes.';
+                        }
+                    }
+                }
+
+                if (!isAdded) {
+                    throw 'Sorry! Internal error is occured.';
+                }
+            })
+            .then(() => {
+                console.log('Transaction success!');
+                return {status: 200, message: "success"};
+            })
+            .catch((e) => {
+                console.log('Transaction failure:', e);
+                return {status: 400, message: e};
+            });
+        } catch (e) {
+            console.log('Transaction failure:', e);
+            return {status: 400, message: e};
+        }
+    } else { // Instagram
+        const InstaRef = admin.firestore().collection('Instagram').doc(account);
+        const myRef = admin.firestore().collection('Users').doc(context.auth.uid);
+    }
+
+    return {status: 400, message: "Unknown error."};
+});
+
+exports.manageFollow = functions.https.onCall(async (data, context) => {
+    const method = data.method; // add, updqte, delete
+    const platform = data.platform;
+    const account = data.account;
+    const title = data.title;
+    const profileURL = data.url;
+
+    // Checking attribute.
+    if (!(typeof method === 'string') || method.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "method" containing the message text to add.');
+    }
+    if (!(typeof platform === 'string') || platform.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "platform" containing the message text to add.');
+    }
+    if (!(typeof account === 'string') || account.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "account" containing the message text to add.');
+    }
+
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+            'while authenticated.');
+    }
+
+    const celebDoc = await admin.firestore().collection('Users').doc(context.auth.uid).get();
+    if (celebDoc.exists) {
+        let celeb = celebDoc.data().celeb || [];
+        if (method == "add") { // add
+            for (let i = 0; i < celeb.length; i++) { // check that celeb already exists.
+                if (celeb[i].account == account && celeb[i].platform == platform) {
+                    return {status: 400, message: "Already exists."};
+                }
+            }
+
+            if (celeb.length < 3) {
+                // transaction
+                const YouTubeRef = admin.firestore().collection('YouTube').doc(account);
+                const myRef = admin.firestore().collection('Users').doc(context.auth.uid);
+                try {
+                    return await admin.firestore().runTransaction(async (t) => {
+                        const YouTubeDoc = await t.get(YouTubeRef);
+                        const myDoc = await t.get(myRef);
+
+                        const now = new Date();
+                        celeb.push({
+                            account: account,
+                            count: 0,
+                            platform: platform,
+                            recent: now,
+                            since: now,
+                            title: title,
+                            url: profileURL
+                        });
+
+                        if (!YouTubeDoc.exists) {
+                            t.set(YouTubeRef, {count: 0, follow: 1});
+                        } else {
+                            t.update(YouTubeRef, {follow: admin.firestore.FieldValue.increment(1)});
+                        }
+
+                        t.update(myRef, {celeb: celeb});
+                    })
+                    .then(() => {
+                        console.log('Transaction success!');
+                        return {status: 200, message: "success"};
+                    })
+                    .catch((e) => {
+                        console.log('Transaction failure:', e);
+                        return {status: 400, message: e};
+                    });
+                } catch (e) {
+                    return {status: 400, message: e};
+                }
+            } else {
+                return {status: 400, message: "Add limit: 3"}
+            }
+        } else if (method == "update") { // update
+            for (let i = 0; i < celeb.length; i++) {
+                if (celeb[i].account == account && celeb[i].platform == platform) {
+                    celeb[i].title = title;
+                    celeb[i].url = profileURL;
+                    return await admin.firestore().collection('Users').doc(context.auth.uid).update({celeb: celeb})
+                    .then(() => {
+                        return {status: 200, message: "success"};
+                    })
+                    .catch((e) => {
+                        return {status: 400, message: e};
+                    });
+                }
+            }
+        } else if (method == "delete") { // delete
+            // transaction
+            const YouTubeRef = admin.firestore().collection('YouTube').doc(account);
+            const myRef = admin.firestore().collection('Users').doc(context.auth.uid);
+            try {
+                return await admin.firestore().runTransaction(async (t) => {
+                    const YouTubeDoc = await t.get(YouTubeRef);
+                    const myDoc = await t.get(myRef);
+
+                    for (let i = 0; i < celeb.length; i++) {
+                        if (celeb[i].account == account && celeb[i].platform == platform) {
+                            t.update(YouTubeRef, {count: YouTubeDoc.data().count - celeb[i].count, follow: YouTubeDoc.data().follow - 1});
+
+                            celeb = celeb.filter(el => !(el.account == account && el.platform == platform));
+                            t.update(myRef, {celeb: celeb});
+                            break;
+                        }
+                    }
+                })
+                .then(() => {
+                    console.log('Transaction success!');
+                    return {status: 200, message: "success"};
+                })
+                .catch((e) => {
+                    console.log('Transaction failure:', e);
+                    return {status: 400, message: e};
+                });
+            } catch (e) {
+                console.log('Transaction failure:', e);
+                return {status: 400, message: e};
+            }
+        }
+    } else {
+        return {status: 400, message: "Your data doesn't exist."}
+    }
+
+    return {status: 400, message: "Unknown error"}
+});
+
+exports.initUser = functions.https.onCall(async (data, context) => {
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+            'while authenticated.');
+    }
+
+    let myDoc = admin.firestore().collection('Users').doc(context.auth.uid);
+    let snapshot = await myDoc.get();
+
+    if (!snapshot.exists) {
+        return await myDoc.set({celeb: []})
+            .then(() => {
+                return {status: 200, message: "success"};
+            })
+            .catch((e) => {
+                return {status: 400, message: e};
+            });
+    } else {
+        return {status: 400, message: "User data already exists."};
+    }
+
+    return {status: 400, message: "Unknown error."};
+});
+
+exports.deleteAccount = functions.https.onCall(async (data, context) => {
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+            'while authenticated.');
+    }
+
+    // transaction
+    const YouTubeDocs = [];
+    const myRef = admin.firestore().collection('Users').doc(context.auth.uid);
+
+    const celebDoc = await myRef.get();
+    let celeb = celebDoc.data().celeb || [];
+    
+    try {
+        return await admin.firestore().runTransaction(async (t) => {
+            for (let i = 0; i < celeb.length; i++) {
+                const YouTubeRef = admin.firestore().collection('YouTube').doc(celeb[i].account);
+                YouTubeDocs.push(await t.get(YouTubeRef));
+            }
+            const snapshot = await t.get(myRef);
+
+            for (let i = 0; i < celeb.length; i++) {
+                t.update(YouTubeDocs[i].ref, {count: YouTubeDocs[i].data().count - celeb[i].count, follow: YouTubeDocs[i].data().follow - 1});
+            }
+
+            if (snapshot.exists) {
+                t.delete(myRef);
+            }
+        })
+        .then(() => {
+            console.log('Transaction success!');
+            return {status: 200, message: "success"};
+        })
+        .catch((e) => {
+            console.log('Transaction failure:', e);
+            return {status: 400, message: e};
+        });
+    } catch (e) {
+        console.log('Transaction failure:', e);
+        return {status: 400, message: e};
+    }
+
+    return {status: 400, message: "Unknown error."};
+});
